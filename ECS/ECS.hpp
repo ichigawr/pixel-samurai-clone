8caceae86d5e6cad614b7633db3a2ec36680cb5c
ichigoih <nongami87@gmail.com>
2024-03-20 20:52:30 +0700

    render layers and grouping

--- ECS/ECS.hpp
+++ ECS/ECS.hpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 #include <vector>
-#include <memory>
 #include <algorithm>
+#include <memory>
 #include <bitset>
 #include <array>
 
@@ -12,26 +12,28 @@ class Component;
 class Entity;
 class Manager;
 
-
 using ComponentID = std::size_t;
+using Group = std::size_t;
 
 
-inline ComponentID getComponentTypeID() {
+inline ComponentID getNewComponentTypeID() {
     static ComponentID lastID = 0u;
     return lastID++;
 }
 
 
 template <typename T> inline ComponentID getComponentTypeID() noexcept {
-    static ComponentID typeID = getComponentTypeID();
+    static ComponentID typeID = getNewComponentTypeID();
     return typeID;
 }
 
 
 constexpr std::size_t maxComponents = 32;
+constexpr std::size_t maxGroups = 32;
 
+using ComponentBitset = std::bitset<maxComponents>;
+using GroupBitset = std::bitset<maxGroups>;
 
-using ComponentBitSet = std::bitset<maxComponents>;
 using ComponentArray = std::array<Component*, maxComponents>;
 
 
@@ -49,13 +51,18 @@ class Component {
 
 class Entity {
     private:
+        Manager& manager;
         bool active = true;
         std::vector<std::unique_ptr<Component>> components;
 
         ComponentArray componentArray;
-        ComponentBitSet componentBitSet;
+        ComponentBitset componentBitset;
+        GroupBitset groupBitset;
 
     public:
+        Entity(Manager& mManager)
+            : manager(mManager) {    }
+
         void update() {
             for (auto& c : components)
                 c->update();
@@ -74,8 +81,18 @@ class Entity {
             active = false;
         }
 
+        bool hasGroup(Group mGroup) {
+            return groupBitset[mGroup];
+        }
+
+        void addGroup(Group mGroup);
+
+        void delGroup(Group mGroup) {
+            groupBitset[mGroup] = false;
+        }
+
         template <typename T> bool hasComponent() const {
-            return componentBitSet[getComponentTypeID<T>()];
+            return componentBitset[getComponentTypeID<T>()];
         }
 
         template <typename T, typename... TArgs>
@@ -86,7 +103,7 @@ class Entity {
             components.emplace_back(std::move(uPtr));
 
             componentArray[getComponentTypeID<T>()] = c;
-            componentBitSet[getComponentTypeID<T>()] = true;
+            componentBitset[getComponentTypeID<T>()] = true;
 
             c->init();
             return *c;
@@ -102,6 +119,7 @@ class Entity {
 class Manager {
     private:
         std::vector<std::unique_ptr<Entity>> entities;
+        std::array<std::vector<Entity*>, maxGroups> groupedEntities;
 
     public:
         void update() {
@@ -115,13 +133,29 @@ class Manager {
         }
 
         void refresh() {
+            for (auto i = 0u; i < maxGroups; i++) {
+                auto& v(groupedEntities[i]);
+
+                v.erase(std::remove_if(std::begin(v), std::end(v), [i](Entity* mEntity) {
+                    return !mEntity->isActive() || !mEntity->hasGroup(i);
+                }), std::end(v));
+            }
+            
             entities.erase(std::remove_if(std::begin(entities), std::end(entities), [](const std::unique_ptr<Entity> &mEntity) {
                 return !mEntity->isActive();
             }), std::end(entities));
         }
 
+        void AddToGroup(Entity* mEntity, Group mGroup) {
+            groupedEntities[mGroup].emplace_back(mEntity);
+        }
+
+        std::vector<Entity*>& getGroup(Group mGroup) {
+            return groupedEntities[mGroup];
+        }        
+
         Entity& addEntity() {
-            Entity *e = new Entity();
+            Entity *e = new Entity(*this);
             std::unique_ptr<Entity> uPtr{e};
             entities.emplace_back(std::move(uPtr));
             return *e;
--- Game.cpp
+++ Game.cpp
@@ -17,6 +17,13 @@ std::vector<ColliderComponent*> Game::colliders;
 auto& player(manager.addEntity());
 auto& wall(manager.addEntity());
 
+enum groupLabels : std::size_t {
+    groupMap,
+    groupPlayers,
+    groupEnemies,
+    groupColliders
+};
+
 Game::Game() {    }
 Game::~Game() {    }
 
@@ -46,10 +53,12 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
     player.addComponent<SpriteComponent>("assets/player.png");
     player.addComponent<KeyboardController>();
     player.addComponent<ColliderComponent>("player");
+    player.addGroup(groupPlayers);
 
     wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
     wall.addComponent<SpriteComponent>("assets/dirt.png");
     wall.addComponent<ColliderComponent>("dirt");
+    wall.addGroup(groupMap);
 }
 
 
@@ -76,9 +85,23 @@ void Game::update() {
 }
 
 
+auto& tiles(manager.getGroup(groupMap));
+auto& players(manager.getGroup(groupPlayers));
+auto& enemies(manager.getGroup(groupEnemies));
+
+
 void Game::render() {
     SDL_RenderClear(renderer);
-    manager.draw();
+
+    for (auto& t : tiles)
+        t->draw();
+
+    for (auto& p : players)
+        p->draw();
+
+    for (auto& e : enemies)
+        e->draw();
+
     SDL_RenderPresent(renderer);
 }
 
@@ -93,4 +116,5 @@ void Game::clean() {
 void Game::AddTile(int id, int x, int y) {
     auto& tile(manager.addEntity());
     tile.addComponent<TileComponent>(x, y, 32, 32, id);
+    tile.addGroup(groupMap);
 }
